54afee1e6302c14d36af244481cc79d51bb9060a
Matt Oztalay <ozzmeister00@gmail.com>
2018-01-31 17:21:43 -0600

    First pass at proper horizontal velocity kill
    toggle the brakes, and it'll come to a stop (eventually). Still needs
    work.

--- hover.py
+++ hover.py
@@ -1,8 +1,6 @@
 import krpc
-from math import cos, sin
+from math import cos, sin, radians, degrees, atan2, asin
 import time
-
-#from krpc.utils import PID, hasAborted
 
 connection = krpc.connect("Hover")
 vessel = connection.space_center.active_vessel
@@ -12,7 +10,7 @@ g = vessel.orbit.body.surface_gravity
 
 horizontalDeflectionMax = 45.0
 horizontalSpeedTolerance = 0.1
-horizontalSpeedMax = 15.0
+horizontalSpeedMax = 25.0
 
 connection.drawing.add_direction((1,0,0), vessel.orbit.body.reference_frame, length=20)
 
@@ -23,11 +21,14 @@ def hasAborted(vessel):
 def main():
     targetAlt = 15.0
 
-    up = 1.0
     midPitch = 0.0
     midYaw = 0.0
 
-    #( 1, 0, 0) <- UP
+    UP = (1, 0, 0)
+
+    vessel.auto_pilot.engage()
+
+    # (1, 0, 0) <- UP
     # (0, 0, 1) <- RIGHT
     # (0, 1, 0) <- BACK
 
@@ -47,34 +48,34 @@ def main():
             if vessel.situation == vessel.situation.landed:
                 break
 
-        # Kill horizontal velocity
-        x, y, z = flight.velocity
+        if vessel.control.brakes:
 
+            # Kill horizontal velocity
+            up, back, right = flight.velocity
 
-        if abs(flight.horizontal_speed) > 1.0:
+            pitchOffset = (right / horizontalSpeedMax) * horizontalDeflectionMax
+            yawOffset = (back / horizontalSpeedMax) * horizontalDeflectionMax
 
-            pitchOffset = (y / 25) * horizontalDeflectionMax
-            yawOffset = (x / 25) * horizontalDeflectionMax
+            # clamp to range
+            pitchOffset = clamp(pitchOffset, -horizontalDeflectionMax, horizontalDeflectionMax)
+            yawOffset = clamp(yawOffset, -horizontalDeflectionMax, horizontalDeflectionMax)
 
-            pitch = midPitch + pitchOffset
-            yaw = midYaw + yawOffset
+            pitch = midPitch - pitchOffset
+            yaw = midYaw - yawOffset
 
-            # https://stackoverflow.com/questions/1568568/how-to-convert-euler-angles-to-directional-vector
+            direction = rpyToDirection(pitch, yaw)
 
-            x = cos(yaw) * cos(pitch)
-            y = sin(yaw) * cos(pitch)
-            z = sin(pitch)
+            vessel.auto_pilot.target_direction = direction
 
-            direction = (x, y, z)
-
-
-            #vessel.control.auto_pilot.target_direction = direction
-
-            connection.drawing.add_direction(direction, vessel.surface_reference_frame)
+            # turn off the brakes if we're broken enough
+            if abs(flight.horizontal_speed) <= horizontalSpeedTolerance:
+                vessel.control.brakes = False
 
         else:
-            connection.drawing.add_direction((1, 0, 0), vessel.surface_reference_frame)
+            vessel.auto_pilot.target_direction = UP
 
+        connection.drawing.add_direction(vessel.direction(vessel.surface_reference_frame), vessel.surface_reference_frame)
+        connection.drawing.add_direction(vessel.auto_pilot.target_direction,vessel.surface_reference_frame)
 
         alt_error = targetAlt - flight.surface_altitude
 
@@ -86,10 +87,27 @@ def main():
 
         time.sleep(0.01)
 
+    vessel.auto_pilot.disengage()
     vessel.control.abort = False
     vessel.control.throttle = 0.0
     vessel.control.sas = True
 
+def rpyToDirection(pitch, yaw):
+    x = cos(radians(yaw)) * cos(radians(pitch))
+    y = sin(radians(yaw)) * cos(radians(pitch))
+    z = sin(radians(pitch))
+
+    return x, y, z
+
+def directionToRPY(direction):
+    x, y, z = direction
+    pitch = degrees(asin(z))
+    yaw = degrees(atan2(y, x))
+
+    return pitch, yaw
+
+def clamp(v, minV, maxV):
+    return max(minV, min(v, maxV))
 
 if __name__ == '__main__':
     main()
