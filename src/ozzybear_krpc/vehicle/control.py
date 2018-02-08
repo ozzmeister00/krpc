773c14cff77ce9df4acc994d70c6b4b92c077454
Tyler Jachetta <me@tylerjachetta.net>
2018-02-07 20:54:15 -0500

    adding some python file stubs in, adding a little bit here and there

---
+++ src/ozzybear_krpc/vehicle/control.py
@@ -0,0 +1,8 @@
+"""
+control.py
+
+Functions for controlling a vehicle, at this time probably mostly rockets.
+This will probably get broken up in the future, so try to group things in what
+ever way seems sensible to you, man.
+"""
+
