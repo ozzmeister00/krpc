6fa885b22d4aeab8b458e632e6a109821e6b0650
Matt Oztalay <ozzmeister00@gmail.com>
2018-03-06 10:28:56 -0600

    PEP8 updates and adding "getLandingReferenceFrame"

--- sketches/sandbox/utils.py
+++ sketches/sandbox/utils.py
@@ -1,19 +1,23 @@
-import krpc
 import time
 from collections import deque
-from math import cos, sin, radians, degrees, atan2, asin
+from math import cos, sin, radians, degrees, atan2, asin, pi
 
 def gHere(body, vessel):
-    return body.surface_gravity * (body.mass / ((vessel.flight(body.reference_frame).mean_altitude + body.equatorial_radius)**2))
+    return body.surface_gravity * (
+    body.mass / ((vessel.flight(body.reference_frame).mean_altitude + body.equatorial_radius) ** 2))
+
 
 def fgHere(body, vessel):
     return vessel.mass * gHere(body, vessel)
 
+
 def hasAborted(vessel):
     return vessel.control.abort
 
+
 def normalizeToRange(v, a, b):
     return (v - a) / (b - a)
+
 
 def rpyToDirection(pitch, yaw):
     x = cos(radians(yaw)) * cos(radians(pitch))
@@ -22,6 +26,7 @@ def rpyToDirection(pitch, yaw):
 
     return x, y, z
 
+
 def directionToRPY(direction):
     x, y, z = direction
     pitch = degrees(asin(z))
@@ -29,13 +34,14 @@ def directionToRPY(direction):
 
     return pitch, yaw
 
+
 class AutoStage(object):
     def __init__(self, vessel):
         self.vessel = vessel
 
     def __call__(self):
         stage = self.vessel.control.current_stage
-        parts = self.vessel.parts.in_decouple_stage(stage-1)
+        parts = self.vessel.parts.in_decouple_stage(stage - 1)
 
         for part in parts:
             engine = part.engine
@@ -73,6 +79,7 @@ class Fairing(object):
 def clamp(v, minV, maxV):
     return max(minV, min(v, maxV))
 
+
 class PID(object):
     def __init__(self, kP=1, kI=0, kD=0, dt=1, cMin=0.0, cMax=1.0):
         self.seekP = 0.0
@@ -107,6 +114,7 @@ class PID(object):
 
         return output
 
+
 class Program(object):
     def __init__(self, prettyName):
         self.prettyName = prettyName
@@ -116,11 +124,33 @@ class Program(object):
         raise NotImplementedError("Sublcass of Program has not been set up correctly. Implement a __call__ method.")
 
     def displayValues(self):
-        raise NotImplementedError("Sublcass of Program has not been set up correctly. Implement a displayValues method.")
+        raise NotImplementedError(
+            "Sublcass of Program has not been set up correctly. Implement a displayValues method.")
 
+def getLandingReferenceFrame(connection, body, vessel, landing_longitude, landing_latitude):
 
-def setupConsole():
-    connection = krpc.connect("Console")
-    vessel = connection.space_center.active_vessel
-    ut = connection.add_stream(getattr, connection.space_center, 'ut')
-    return connection, vessel, ut
+    ReferenceFrame = connection.space_center.ReferenceFrame
+
+    # Define landing site
+    landing_latitude = -(0+(5.0/60)+(48.38/60/60)) # Top of the VAB
+    landing_longitude = -(74+(37.0/60)+(12.2/60/60))
+    landing_altitude = 2000
+
+    # Determine landing site reference frame (orientation: x=zenith, y=north, z=east)
+    landing_position = body.surface_position(landing_latitude, landing_longitude, body.reference_frame)
+    q_long = (0, sin(-landing_longitude * 0.5 * pi / 180), 0, cos(-landing_longitude * 0.5 * pi / 180))
+    q_lat = (0, 0, sin(landing_latitude * 0.5 * pi / 180), cos(landing_latitude * 0.5 * pi / 180))
+    landing_reference_frame = ReferenceFrame.create_relative(
+                                ReferenceFrame.create_relative(
+                                  ReferenceFrame.create_relative(
+                                    body.reference_frame,
+                                    landing_position,
+                                    q_long),
+                                (0,0,0),
+                                q_lat),
+                                (landing_altitude, 0, 0)
+                                )
+
+    # Up, North, East
+    vessel.velocity(landing_reference_frame)
+    return landing_reference_frame
